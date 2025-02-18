f26c717b37db823ef2d824563e6dfc06473c7aa8
92246 <92246@lulumea.com>
2025-02-18 22:35:04 +0530

    initial commit 15/02

--- lib/history_page.dart
+++ lib/history_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HistoryPage extends StatelessWidget {
+  const HistoryPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
